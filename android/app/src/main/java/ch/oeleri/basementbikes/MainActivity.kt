b9c49565022d146946d4d6eeeda680634edf9529
Christopher Renaud Oelerich <chris@oeleri.ch>
2025-01-04 22:29:06 -0500

    Update namespace

--- android/app/src/main/java/ch/oeleri/basementbikes/MainActivity.kt
+++ android/app/src/main/java/ch/oeleri/basementbikes/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anonymous.basementbikes
+package ch.oeleri.basementbikes
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/ch/oeleri/basementbikes/MainApplication.kt
+++ android/app/src/main/java/ch/oeleri/basementbikes/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.anonymous.basementbikes
+package ch.oeleri.basementbikes
 
 import android.app.Application
 import android.content.res.Configuration
